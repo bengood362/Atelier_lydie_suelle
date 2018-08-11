9c7ff1388c89aed242ff20f92e9c10ced0df9ed7
Ka Yin Chan <bengood362@gmail.com>
2018-08-11 13:39:58 +0800

    +print board has padding as option +attr CW & ACW rotation, is_equal +attr print +board apply attr

--- Atelier_core.py
+++ Atelier_core.py
@@ -11,19 +11,20 @@ class BonusColor:
       raise Err("BonusColor:__init__: No such color: {0}".format(bonus_color))
     self.bonus_color = bonus_color
     self.bonus_value = bonus_value
-  def getf(self):
+  def getf(self, padding=1):
     if(self.bonus_color == 'None'):
-      return ' '
+      return ' '*padding
     else:
-      return self.bonus_color[:1]
-  def getv(self):
+      res = self.bonus_type[:padding]
+      return res + ' '*(padding-len(res))
+  def getv(self, padding=2):
     if(self.bonus_color == 'None'):
-      return '  '
+      return ' '*padding
     else:
       if (len(str(self.bonus_value)) == 2):
-        return str(self.bonus_value)
+        return ' '*(padding-2)+str(self.bonus_value)
       else:
-        return ' {0}'.format(self.bonus_value)
+        return ' '*(padding-1)+'{0}'.format(self.bonus_value)
 
 class BonusType:
   # Currently quality and inheritance are not in use
@@ -33,19 +34,20 @@ class BonusType:
       raise Err("BonusType:__init__: No such type: {0}".format(bonus_type))
     self.bonus_type = bonus_type
     self.bonus_value = bonus_value
-  def getf(self):
+  def getf(self, padding=2):
     if(self.bonus_type == 'None'):
-      return '  '
+      return ' '*padding
     else:
-      return self.bonus_type[:2]
-  def getv(self):
+      res = self.bonus_type[:padding]
+      return res + ' '*(padding-len(res))
+  def getv(self, padding=2):
     if(self.bonus_type == 'None'):
-      return '  '
+      return ' '*padding
     else:
       if (len(str(self.bonus_value)) == 2):
-        return str(self.bonus_value)
+        return ' '*(padding-2)+str(self.bonus_value)
       else:
-        return ' {0}'.format(self.bonus_value)
+        return ' '*(padding-1)+'{0}'.format(self.bonus_value)
 
 class BoardGrid:
   def __init__(self, bonus_color=None, bonus_type=None, current_color=None):
@@ -56,31 +58,39 @@ class BoardGrid:
     self.bonus_color = bonus_color
   def add_type(self, bonus_type):
     self.bonus_type = bonus_type
-  def getcc(self):
+  def getcc(self, padding=1):
     if(self.bonus_color):
-      return self.bonus_color.getf()
+      return self.bonus_color.getf(padding)
     else:
-      return ' '
-  def getcv(self):
+      return ' '*padding
+  def getcv(self, padding=2):
     if(self.bonus_color):
-      return self.bonus_color.getv()
+      return self.bonus_color.getv(padding)
     else:
-      return '  '
-  def gettc(self):
+      return ' '*padding
+  def gettc(self, padding=2):
     if(self.bonus_type):
-      return self.bonus_type.getf()
+      return self.bonus_type.getf(padding)
     else:
-      return '  '
-  def gettv(self):
+      return ' '*padding
+  def gettv(self, padding=2):
     if(self.bonus_type):
-      return self.bonus_type.getv()
+      return self.bonus_type.getv(padding)
     else:
-      return '  '
+      return ' '*padding
+  def __str__(self):
+    return ' ' if self.current_color is None else self.current_color
+  def set_current_color(self, color):
+    self.current_color = color
+    return self
+  def get_current_color(self, padding=2):
+    return (' '*padding) if self.current_color is None else (self.current_color+' '*(padding-1))
 
+# x: height, y: width, different with real world's mathematics, because I am bad at programming
 class Board:
   def __init__(self, shape=4, bonus_colors=[], bonus_types=[], requirements=[]):
-    # NOTE: example of bonus color: ((coordX, coordY), (color, value))
-    self.shape=shape
+    # NOTE: example of bonus color: ((coordX, coordY), (color, bonus_value))
+    self.shape = shape
     self.board = []
     for j in range(shape):
       tmp = []
@@ -101,6 +111,7 @@ class Board:
     self.requirements = requirements
   def add_requirement(self, requirement):
     self.requirements.append(requirement)
+    return self
   def test_requirement(self):
     for req in self.requirements:
       pass
@@ -108,40 +119,68 @@ class Board:
   def calculate_score(self):
     pass
   # 2x2 ONLY
-  def print_color_board(self):
+  def print_bonus_color_board(self, padding=2):
     for j in range(self.shape):
       code_buffer = '|'
       for i in range(self.shape):
-        code_buffer += self.board[j][i].getcc()
+        code_buffer += self.board[j][i].getcc(padding=padding)
         code_buffer += ' |'
       val_buffer = '|'
       for i in range(self.shape):
-        val_buffer += self.board[j][i].getcv()
+        val_buffer += self.board[j][i].getcv(padding=padding)
         val_buffer += '|'
-      print '+--'*(len(code_buffer)/3)+'+'
+      print ('+'+'-'*padding)*(len(code_buffer)/(padding+1))+'+'
       print code_buffer
       print val_buffer
-    print '+--'*(len(code_buffer)/3)+'+'
+    print ('+'+'-'*padding)*(len(code_buffer)/(padding+1))+'+'
   # 2x2 ONLY
-  def print_type_board(self):
+  def print_bonus_type_board(self, padding=2):
     for j in range(self.shape):
       code_buffer = '|'
       for i in range(self.shape):
-        code_buffer += self.board[j][i].gettc()
+        code_buffer += self.board[j][i].gettc(padding=padding)
         code_buffer += '|'
       val_buffer = '|'
       for i in range(self.shape):
-        val_buffer += self.board[j][i].gettv()
+        val_buffer += self.board[j][i].gettv(padding=padding)
         val_buffer += '|'
-      print '+--'*(len(code_buffer)/3)+'+'
+      print ('+'+'-'*padding)*(len(code_buffer)/(padding+1))+'+'
       print code_buffer
       print val_buffer
-    print '+--'*(len(code_buffer)/3)+'+'
-
+    print ('+'+'-'*padding)*(len(code_buffer)/(padding+1))+'+'
+  def print_board(self, padding=2):
+    for j in range(self.shape):
+      code_buffer = '|'
+      for i in range(self.shape):
+        code_buffer += self.board[j][i].get_current_color(padding=padding)
+        code_buffer += '|'
+      val_buffer = '|'
+      for i in range(self.shape):
+        val_buffer += ' '*padding
+        val_buffer += '|'
+      print ('+'+'-'*padding)*(len(code_buffer)/(padding+1))+'+'
+      print code_buffer
+      print val_buffer
+    print ('+'+'-'*padding)*(len(code_buffer)/(padding+1))+'+'
+  def get_possible_offset(self, attr):
+    return (self.shape-attr.height, self.shape-attr.width)
+  def get_shape(self):
+    return (self.shape, self.shape)
+  def apply_attr(self, attr, (offset_x, offset_y)):
+    # offset: [coordX, coordY]
+    if(((offset_x + attr.height) > self.shape) or ((offset_y + attr.width) > self.shape)):
+      raise Err("Board:apply_attr: Cannot have offset ({0}, {1})".format(offset_x, offset_y))
+    for (x,y) in attr.coords:
+      self.board[x+offset_x][y+offset_y].set_current_color(attr.color)
+    return self
 # ingredient
 class Attribute:
+  possible_color = {'r': 1, 'g': 2, 'b': 3, 'y': 4, 'p': 5}
+  # + width, height
   def __init__(self, color='r', coords=[]):
     # NOTE: coords: [(coordX1, coordY1), (coordX2, coordY2)]
+    if(color not in Attribute.possible_color):
+      raise Err("Attribute:__init__: No such color: {0}".format(color))
     maxX = max(zip(*coords)[0])
     maxY = max(zip(*coords)[1])
     minX = min(zip(*coords)[0])
@@ -168,10 +207,45 @@ class Attribute:
       new_y = x
       new_coords.append((new_x, new_y))
     return Attribute(color=self.color, coords=new_coords)
+  def is_equal(self, attr):
+    coords_self = self.coords[:]
+    coords_other = attr.coords[:]
+    color_self = self.color
+    color_other = attr.color
+    if(color_self != color_other):
+      return False
+    for coord_self in coords_self:
+      for coord_other in coords_other:
+        try:
+          coords_self.remove(coord_other)
+        except ValueError:
+          return False
+    if(len(coords_self) == 0):
+      return True
+    else:
+      return False
+  def get_all_rotation(self):
+    result=[self]
+    attr1=self.CW_rotate()
+    attr2=attr1.CW_rotate()
+    attr3=attr2.CW_rotate()
+    # The result can either be [1, 2, 4]
+    if(self.is_equal(attr1)):
+      return [self]
+    elif(self.is_equal(attr2)):
+      return [self, attr1]
+    else:
+      return [self, attr1, attr2, attr3]
   def print_attr(self):
-    print self.color
-    for coord in self.coords:
-      print coord
+    s = '+'+'-'*self.width+'+\n'
+    for i in range(self.height):
+      s += '|'
+      for j in range(self.width):
+        s += self.color if (i,j) in self.coords else '.'
+      s += '|\n'
+    s += '+'+'-'*self.width+'+'
+    print(s)
+    return self
 
 class Ingredient:
   def __init__(self, attributes=[]):
--- Atelier_test.py
+++ Atelier_test.py
@@ -1,6 +1,6 @@
 # Atelier_test.py
 import Atelier_core as Atelier
-
+# ------------- Check gameboard ------------- #
 bonus_color = [
   ((0,0),('r',1)),
   ((1,1),('r',1)),
@@ -15,14 +15,49 @@ board = Atelier.Board(shape=4, bonus_colors=bonus_color, bonus_types=bonus_type)
 # print ("\n###########################")
 # print ("# This is the color_board #")
 # print ("###########################\n")
-# board.print_color_board()
+# board.print_bonus_color_board()
 # print ("\n###########################")
 # print ("# This is the  type_board #")
 # print ("###########################\n")
-# board.print_type_board()
+# board.print_bonus_type_board()
+# ------------- End check gameboard ------------- #
 
+# ------------- Check attribute ------------- #
 attr = Atelier.Attribute(color='r',coords=[(0,1),(1,3),(2,0),(2,3)])
-new_attr = attr.CW_rotate()
-new_attr.print_attr()
-new_attr2 = attr.ACW_rotate()
-new_attr2.print_attr()
+# new_attr = attr.CW_rotate()
+# new_attr.print_attr()
+# new_attr2 = attr.ACW_rotate()
+# new_attr2.print_attr()
+# attr2 = Atelier.Attribute(color='r',coords=[(0,1),(1,3),(2,0),(2,3)])
+# attr3 = Atelier.Attribute(color='y',coords=[(0,1),(1,3),(2,0),(2,3)])
+# attr4 = Atelier.Attribute(color='r',coords=[(0,1),(1,3),(2,0),(2,3),(0,0)])
+# attr5 = Atelier.Attribute(color='r',coords=[(0,1),(1,3),(2,0),])
+# assert attr.is_equal(attr2), "attr should be same as attr2"
+# assert (not attr.is_equal(attr3)), "attr should have different color with attr3"
+# assert (not attr.is_equal(attr4)), "attr have 1 less element than attr4"
+# assert (not attr.is_equal(attr5)), "attr have 1 more element than attr5"
+# print ("Should have 4 rotation type")
+# for a in attr.get_all_rotation():
+#   a.print_attr()
+# print ("Should have 2 rotation type")
+attr6 = Atelier.Attribute(color='y',coords=[(0,0),(1,1)])
+# for a in attr6.get_all_rotation():
+#   a.print_attr()
+attr7 = Atelier.Attribute(color='y',coords=[(0,0)])
+# print ("Should have 1 rotation type")
+# for a in attr7.get_all_rotation():
+#   a.print_attr()
+attr8 = Atelier.Attribute(color='r',coords=[(0,0),(0,1),(1,0)])
+# print 'board shape: {0}'.format(board.get_shape())
+# attr.print_attr()
+# print board.get_possible_offset(attr)
+# attr6.print_attr()
+# print board.get_possible_offset(attr6)
+# attr7.print_attr()
+# print board.get_possible_offset(attr7)
+# board.apply_attr(attr7, (4,4)) # Should have error
+board.apply_attr(attr8, (1,1))
+board.apply_attr(attr6, (0,0))
+board.apply_attr(attr7, (3,3))
+board.print_board()
+# ------------- End check attribute ------------- #
